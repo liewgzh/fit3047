a88d2810876389686f0618acfe666aa7425c2a3d
Gordon Liew <glie0003@student.monash.edu>
2024-04-14 16:56:29 +1000

    sbadmin frontend 1

--- templates/Appointments/index.php
+++ templates/Appointments/index.php
@@ -3,26 +3,34 @@
  * @var \App\View\AppView $this
  * @var iterable<\App\Model\Entity\Appointment> $appointments
  */
+ echo $this->Html->css('/vendor/datatables/dataTables.bootstrap4.min.css', ['block' => true]);
+ echo $this->Html->script('/vendor/datatables/jquery.dataTables.min.js', ['block' => true]);
+ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['block' => true]);
 ?>
 <div class="appointments index content">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><?= __('Appointments') ?></h1>
+        <a href="<?= $this->Url->build(['action' => 'add']) ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-plus fa-sm text-white-50"></i> New Appointment</a>
+    </div>
     <?= $this->Html->link(__('New Appointment'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Appointments') ?></h3>
     <div class="table-responsive">
-        <table>
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
             <thead>
                 <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('client_id') ?></th>
-                    <th><?= $this->Paginator->sort('guest_name') ?></th>
-                    <th><?= $this->Paginator->sort('guest_email') ?></th>
-                    <th><?= $this->Paginator->sort('counsellor_id') ?></th>
-                    <th><?= $this->Paginator->sort('service_id') ?></th>
-                    <th><?= $this->Paginator->sort('appointment_date') ?></th>
-                    <th><?= $this->Paginator->sort('start_time') ?></th>
-                    <th><?= $this->Paginator->sort('end_time') ?></th>
-                    <th><?= $this->Paginator->sort('appointment_status') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
+                    <th><?= h('id') ?></th>
+                    <th><?= h('client_id') ?></th>
+                    <th><?= h('guest_name') ?></th>
+                    <th><?= h('guest_email') ?></th>
+                    <th><?= h('counsellor_id') ?></th>
+                    <th><?= h('service_id') ?></th>
+                    <th><?= h('appointment_date') ?></th>
+                    <th><?= h('start_time') ?></th>
+                    <th><?= h('end_time') ?></th>
+                    <th><?= h('appointment_status') ?></th>
+                    <th><?= h('created') ?></th>
+                    <th><?= h('modified') ?></th>
                     <th class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
@@ -51,14 +59,9 @@
             </tbody>
         </table>
     </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
+    <script>
+        $(document).ready(function() {
+          $('#dataTable').DataTable();
+        });
+    </script>>
 </div>
