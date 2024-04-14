e15f55452156d66a52279ea178fc221e5387fef6
yunxi liu <yliu0431@student.monash.edu>
2024-04-15 02:00:43 +1000

    homepage with counsellor, about us and seminar and a navigation bar on the top

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -59,11 +59,20 @@ endif;
 ?>
 <!DOCTYPE html>
 <html>
+<nav class="top-nav">
+    <ul>
+        <li><a href="#" data-content="counselor">Counsellor</a></li>
+        <li><a href="#" data-content="about">About</a></li>
+        <li><a href="#" data-content="seminar">Seminar</a></li>
+        <li><a href="<?= $this->Url->build(['controller' => 'Appointments', 'action' => 'add']) ?>" class="book-button">Book</a></li>
+    </ul>
+</nav>
+
 <head>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        CakePHP: the rapid development PHP framework:
+        Calm Wellness Center
         <?= $this->fetch('title') ?>
     </title>
     <?= $this->Html->meta('icon') ?>
@@ -77,163 +86,133 @@ endif;
 <body>
     <header>
         <div class="container text-center">
-            <a href="https://cakephp.org/" target="_blank" rel="noopener">
-                <img alt="CakePHP" src="https://cakephp.org/v2/img/logos/CakePHP_Logo.svg" width="350" />
-            </a>
-            <h1>
-                Welcome to CakePHP <?= h(Configure::version()) ?> Chiffon (🍰)
-            </h1>
+<!--            <a href="https://cakephp.org/" target="_blank" rel="noopener">-->
+<!--                <img alt="CakePHP" src="https://cakephp.org/v2/img/logos/CakePHP_Logo.svg" width="350" />-->
+<!--            </a>-->
+            <h2>
+                Calm Wellness Center
+            </h2>
         </div>
     </header>
     <main class="main">
         <div class="container">
-            <div class="content">
-                <div class="row">
-                    <div class="column">
-                        <div class="message default text-center">
-                            <small>Please be aware that this page will not be shown if you turn off debug mode unless you replace templates/Pages/home.php with your own version.</small>
+            <div id="counselor-content" class="content-section">
+                <section class="counselors">
+                    <div class="row">
+                        <!-- Counselor 1 -->
+                        <div class="column">
+                            <?= $this->Html->image('nice_to_meet_u.jpg', ['alt' => 'Counselor image']) ?>
+                            <h3>Counselor 1</h3>
+                            <p>Short description or bio of the counselor.</p>
+                            <?= $this->Html->link('Learn More', '/counselor-profile', ['class' => 'button']) ?>
                         </div>
-                        <div id="url-rewriting-warning" style="padding: 1rem; background: #fcebea; color: #cc1f1a; border-color: #ef5753;">
-                            <ul>
-                                <li class="bullet problem">
-                                    URL rewriting is not properly configured on your server.<br />
-                                    1) <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/en/installation.html#url-rewriting">Help me configure it</a><br />
-                                    2) <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/en/development/configuration.html#general-configuration">I don't / can't use URL rewriting</a>
-                                </li>
-                            </ul>
+                        <div class="column">
+                            <?= $this->Html->image('nice_to_meet_u.jpg', ['alt' => 'Counselor image']) ?>
+                            <h3>Counselor 2</h3>
+                            <p>Short description or bio of the counselor.</p>
+                            <?= $this->Html->link('Learn More', '/counselor-profile', ['class' => 'button']) ?>
                         </div>
-                        <?php Debugger::checkSecurityKeys(); ?>
+                        <div class="column">
+                            <?= $this->Html->image('nice_to_meet_u.jpg', ['alt' => 'Counselor image']) ?>
+                            <h3>Counselor 3</h3>
+                            <p>Short description or bio of the counselor.</p>
+                            <?= $this->Html->link('Learn More', '/counselor-profile', ['class' => 'button']) ?>
+                        </div>
+                        <div class="column">
+                            <?= $this->Html->image('nice_to_meet_u.jpg', ['alt' => 'Counselor image']) ?>
+                            <h3>Counselor 4</h3>
+                            <p>Short description or bio of the counselor.</p>
+                            <?= $this->Html->link('Learn More', '/counselor-profile', ['class' => 'button']) ?>
+                        </div>
+                        <div class="column">
+                            <?= $this->Html->image('nice_to_meet_u.jpg', ['alt' => 'Counselor image']) ?>
+                            <h3>Counselor 5</h3>
+                            <p>Short description or bio of the counselor.</p>
+                            <?= $this->Html->link('Learn More', '/counselor-profile', ['class' => 'button']) ?>
+                        </div>
+                        <div class="column">
+                            <?= $this->Html->image('nice_to_meet_u.jpg', ['alt' => 'Counselor image']) ?>
+                            <h3>Counselor 6</h3>
+                            <p>Short description or bio of the counselor.</p>
+                            <?= $this->Html->link('Learn More', '/counselor-profile', ['class' => 'button']) ?>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="column">
-                        <h4>Environment</h4>
-                        <ul>
-                        <?php if (version_compare(PHP_VERSION, '8.1.0', '>=')) : ?>
-                            <li class="bullet success">Your version of PHP is 8.1.0 or higher (detected <?= PHP_VERSION ?>).</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your version of PHP is too low. You need PHP 8.1.0 or higher to use CakePHP (detected <?= PHP_VERSION ?>).</li>
-                        <?php endif; ?>
-
-                        <?php if (extension_loaded('mbstring')) : ?>
-                            <li class="bullet success">Your version of PHP has the mbstring extension loaded.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your version of PHP does NOT have the mbstring extension loaded.</li>
-                        <?php endif; ?>
-
-                        <?php if (extension_loaded('openssl')) : ?>
-                            <li class="bullet success">Your version of PHP has the openssl extension loaded.</li>
-                        <?php elseif (extension_loaded('mcrypt')) : ?>
-                            <li class="bullet success">Your version of PHP has the mcrypt extension loaded.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your version of PHP does NOT have the openssl or mcrypt extension loaded.</li>
-                        <?php endif; ?>
-
-                        <?php if (extension_loaded('intl')) : ?>
-                            <li class="bullet success">Your version of PHP has the intl extension loaded.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your version of PHP does NOT have the intl extension loaded.</li>
-                        <?php endif; ?>
-                        </ul>
-                    </div>
-                    <div class="column">
-                        <h4>Filesystem</h4>
-                        <ul>
-                        <?php if (is_writable(TMP)) : ?>
-                            <li class="bullet success">Your tmp directory is writable.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your tmp directory is NOT writable.</li>
-                        <?php endif; ?>
-
-                        <?php if (is_writable(LOGS)) : ?>
-                            <li class="bullet success">Your logs directory is writable.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your logs directory is NOT writable.</li>
-                        <?php endif; ?>
-
-                        <?php $settings = Cache::getConfig('_cake_core_'); ?>
-                        <?php if (!empty($settings)) : ?>
-                            <li class="bullet success">The <em><?= h($settings['className']) ?></em> is being used for core caching. To change the config edit config/app.php</li>
-                        <?php else : ?>
-                            <li class="bullet problem">Your cache is NOT working. Please check the settings in config/app.php</li>
-                        <?php endif; ?>
-                        </ul>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="column">
-                        <h4>Database</h4>
-                        <?php
-                        $result = $checkConnection('default');
-                        ?>
-                        <ul>
-                        <?php if ($result['connected']) : ?>
-                            <li class="bullet success">CakePHP is able to connect to the database.</li>
-                        <?php else : ?>
-                            <li class="bullet problem">CakePHP is NOT able to connect to the database.<br /><?= h($result['error']) ?></li>
-                        <?php endif; ?>
-                        </ul>
-                    </div>
-                    <div class="column">
-                        <h4>DebugKit</h4>
-                        <ul>
-                        <?php if (Plugin::isLoaded('DebugKit')) : ?>
-                            <li class="bullet success">DebugKit is loaded.</li>
-                            <?php
-                            $result = $checkConnection('debug_kit');
-                            ?>
-                            <?php if ($result['connected']) : ?>
-                                <li class="bullet success">DebugKit can connect to the database.</li>
-                            <?php else : ?>
-                                <li class="bullet problem">There are configuration problems present which need to be fixed:<br /><?= $result['error'] ?></li>
-                            <?php endif; ?>
-                        <?php else : ?>
-                            <li class="bullet problem">DebugKit is <strong>not</strong> loaded.</li>
-                        <?php endif; ?>
-                        </ul>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="column links">
-                        <h3>Getting Started</h3>
-                        <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/en/">CakePHP Documentation</a>
-                        <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/en/tutorials-and-examples/cms/installation.html">The 20 min CMS Tutorial</a>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="column links">
-                        <h3>Help and Bug Reports</h3>
-                        <a target="_blank" rel="noopener" href="https://slack-invite.cakephp.org/">Slack</a>
-                        <a target="_blank" rel="noopener" href="https://github.com/cakephp/cakephp/issues">CakePHP Issues</a>
-                        <a target="_blank" rel="noopener" href="https://discourse.cakephp.org/">CakePHP Forum</a>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="column links">
-                        <h3>Docs and Downloads</h3>
-                        <a target="_blank" rel="noopener" href="https://api.cakephp.org/">CakePHP API</a>
-                        <a target="_blank" rel="noopener" href="https://bakery.cakephp.org">The Bakery</a>
-                        <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/en/">CakePHP Documentation</a>
-                        <a target="_blank" rel="noopener" href="https://plugins.cakephp.org">CakePHP plugins repo</a>
-                        <a target="_blank" rel="noopener" href="https://github.com/cakephp/">CakePHP Code</a>
-                        <a target="_blank" rel="noopener" href="https://github.com/FriendsOfCake/awesome-cakephp">CakePHP Awesome List</a>
-                        <a target="_blank" rel="noopener" href="https://www.cakephp.org">CakePHP</a>
-                    </div>
-                </div>
-                <hr>
-                <div class="row">
-                    <div class="column links">
-                        <h3>Training and Certification</h3>
-                        <a target="_blank" rel="noopener" href="https://cakefoundation.org/">Cake Software Foundation</a>
-                        <a target="_blank" rel="noopener" href="https://training.cakephp.org/">CakePHP Training</a>
-                    </div>
-                </div>
+                </section>
             </div>
+            <div id="about-content" class="content-section" style="display: none;">
+                <p>
+                    Client Calm Wellness Centre focuses on conducting counselling sessions with customers,
+                    mostly those with chronic fatigue syndrome, locally, within Australia.
+                    The intent is to keep up with the current business climate and move from pen and paper
+                    to having an online system with a website attracting customers and educating the public.
+                </p>
+            </div>
+            <div id="seminar-content" class="content-section" style="display: none;">
+                <section class="seminars">
+                    <div class="row">
+                        <!-- Counselor 1 -->
+                        <div class="column">
+                            <?= $this->Html->image('cat.jpg', ['alt' => 'Seminar image']) ?>
+                            <h3>Seminar 1</h3>
+                            <p>Short description or bio of the Seminar.</p>
+                            <?= $this->Html->link('Learn More', '/seminar', ['class' => 'button']) ?>
+                        </div>
+                        <div class="column">
+                            <?= $this->Html->image('cat.jpg', ['alt' => 'Seminar image']) ?>
+                            <h3>Seminar 2</h3>
+                            <p>Short description or bio of the Seminar.</p>
+                            <?= $this->Html->link('Learn More', '/seminar', ['class' => 'button']) ?>
+                        </div>
+                        <div class="column">
+                            <?= $this->Html->image('cat.jpg', ['alt' => 'Seminar image']) ?>
+                            <h3>Seminar 3</h3>
+                            <p>Short description or bio of the Seminar.</p>
+                            <?= $this->Html->link('Learn More', '/seminar', ['class' => 'button']) ?>
+                        </div>
+                        <div class="column">
+                            <?= $this->Html->image('cat.jpg', ['alt' => 'Seminar image']) ?>
+                            <h3>Seminar 4</h3>
+                            <p>Short description or bio of the Seminar.</p>
+                            <?= $this->Html->link('Learn More', '/seminar', ['class' => 'button']) ?>
+                        </div>
+                        <div class="column">
+                            <?= $this->Html->image('cat.jpg', ['alt' => 'Seminar image']) ?>
+                            <h3>Seminar 5</h3>
+                            <p>Short description or bio of the Seminar.</p>
+                            <?= $this->Html->link('Learn More', '/seminar', ['class' => 'button']) ?>
+                        </div>
+                        <div class="column">
+                            <?= $this->Html->image('cat.jpg', ['alt' => 'Seminar image']) ?>
+                            <h3>Seminar 6</h3>
+                            <p>Short description or bio of the Seminar.</p>
+                            <?= $this->Html->link('Learn More', '/seminar', ['class' => 'button']) ?>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+
         </div>
     </main>
+
+
 </body>
 </html>
+
+<script>
+    document.addEventListener('DOMContentLoaded', (event) => {
+        document.querySelectorAll('.top-nav a[data-content]').forEach(link => {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+                const contentToShow = this.getAttribute('data-content');
+                // Hide all content sections
+                document.querySelectorAll('.content-section').forEach(section => {
+                    section.style.display = 'none';
+                });
+                // Show the selected content section
+                document.getElementById(contentToShow + '-content').style.display = 'block';
+            });
+        });
+    });
+</script>
+
