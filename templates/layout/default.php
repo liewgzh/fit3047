ea5963729a6be840fd16e206390ab07050f4fdca
Gordon Liew <glie0003@student.monash.edu>
2024-04-17 17:34:00 +1000

    Changed add user to register new account & removed douglas mcgee name next to user icon

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -97,7 +97,7 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Admin Items
+                Information & Management
             </div>
 
             <!-- Nav Item - Users Collapse Menu -->
@@ -111,7 +111,7 @@
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Manage Users</h6>
                         <a class="collapse-item" href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'index']) ?>">View All Users</a>
-                        <a class="collapse-item" href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'add']) ?>">Add New User</a>
+                        <a class="collapse-item" href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'add']) ?>">Register New Account</a>
                     </div>
                 </div>
             </li>
@@ -185,7 +185,6 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
                                 <?=$this->Html->image('undraw_profile.svg', ['class' => 'img-profile rounded-circle']) ?>
                             </a>
                             <!-- Dropdown - User Information -->
